fae82b5f0c7ac34d6fc3c95b17960bd931b8d4a2
yanisOuali21 <92925423+yanisOuali21@users.noreply.github.com>
2021-11-15 09:36:47 +0100

    Add files via upload

---
+++ td5.py
@@ -0,0 +1,80 @@
+def maximun(a,b):
+    if a > b :
+        return (a)
+    else:
+        return ( b )
+    
+    
+def minimun(a,b):
+    if a < b :
+        return (a)
+    else:
+        return ( b )
+    
+    
+    
+def valeur_absolue(x):
+    if x >= 0 :
+        return (x)
+    else:
+        return (x)
+
+
+
+def bowling(boule1,boule2):
+    
+    """
+Description de la fonction : affiche a l ecran les performances d un joueur de bowling boule1 ( int):nombre de quilles renversées avec la premiere boule.
+boule2 (int) : nombre de quilles renversées avec la deuxiéme boule.
+préconditions sur les entrées : boule1 + boule2 <=10
+    """
+    if boule1 == 10 :
+        print ("x")
+    elif boule1+boule2 == 10 :
+        print ("/")
+    elif boule1+boule2 < 10 :
+        print ("total")
+        
+
+def croissant (a,b,c):
+    if a <=b :
+        if  b <= c :
+            resultat = True
+        else :
+                a > b
+    else  :
+        b > c
+    return resultat
+
+def année_bissextille (année):
+   
+        
+                
+
+
+
+
+
+
+
+
+
+
+
+
+def triangle(c1,c2,c3):
+    if c1==c2==c3:
+        print("le triangle est equilateral")
+    elif c1==c2!=c3 :
+        print("le triangle est isocelle")
+    elif  c1!=c2!=c3 :
+        print ( " le triangle est scaléne " )
+        
+        
+        
+    
+    
+    
+    
+
+    

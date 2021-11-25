51fe8270ebf8eb1eca05f3ede0352ddf2e8b1523
yanisOuali21 <92925423+yanisOuali21@users.noreply.github.com>
2021-11-25 14:14:57 +0100

    Add files via upload

--- tp1 yanis ouali.py
+++ tp1 yanis ouali.py
@@ -9,3 +9,21 @@ def bowling(boule1,boule2):
 
     
     
+def molky ( score, gain ):
+    if score + gain == 50 :
+        print ("victoire")
+    elif score +gain >=50:
+        print("25")
+    else:
+        print(score+gain)
+        
+
+def molko(score,gain,nbzerro):
+     if score + gain == 50 :
+        print ("victoire")
+    elif score +gain >=50:
+        print("25")
+    elif nbzerros >= 2:
+        print("perdu")
+    else:
+        print(score+gain)

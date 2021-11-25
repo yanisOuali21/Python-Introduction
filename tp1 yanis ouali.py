a722d26a5e7a69a7c6eed1386e507c98b0ff88f6
yanisOuali21 <92925423+yanisOuali21@users.noreply.github.com>
2021-11-25 14:08:58 +0100

    Add files via upload

---
+++ tp1 yanis ouali.py
@@ -0,0 +1,11 @@
+def bowling(boule1,boule2):
+    if boule1 == 10 :
+        print ("x")
+    elif boule1+boule2 == 10 :
+        print ("/")
+    elif boule1+boule2 < 10 :
+        print ("total")
+
+
+    
+    

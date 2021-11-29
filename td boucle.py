9f2ad4dd10bdf9337f52d5520457371acac1b138
yanisOuali21 <92925423+yanisOuali21@users.noreply.github.com>
2021-11-29 08:07:21 +0100

    Add files via upload

---
+++ td boucle.py
@@ -0,0 +1,59 @@
+for n in range (2,9):
+    print(n)
+
+
+print('____')
+
+
+for c in range (0,25,4):
+    print(c)
+
+
+print("____")
+ 
+machine = "informatique" 
+for lettre in "informatique":
+    print(lettre)
+
+print("____")
+
+
+for maChaine in range (len("informatique")):
+    print(maChaine)
+
+print("____")
+
+
+for MaChaine in range (len("informatique")):
+    print(MaChaine+1)
+
+print("____")
+
+
+#exercice2
+    
+for i in ("azertyuiop"):
+    print("Pour progresser en programmation, la pratique est le plus important")
+    
+    
+print("____")
+
+#exercice3
+
+for table_de_7 in range (0,71,7):
+    print ( table_de_7)
+
+print("____")
+
+for Table_de_7 in range (0,71,7):
+    for i in range (1,11):
+        print ( "7",i,"=", table_de_7)
+print("____")
+
+
+#exercice4
+
+def rechercheOccurence(chaine,c):
+    for lettre in chaine :
+       
+       

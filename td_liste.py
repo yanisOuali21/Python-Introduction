b7895ec7b5ecf365a490e48e33104e5355c59992
yanisOuali21 <92925423+yanisOuali21@users.noreply.github.com>
2022-01-10 09:46:40 +0100

    Add files via upload

---
+++ td_liste.py
@@ -0,0 +1,21 @@
+#exercice2
+liste = ['alice','bob','tom']
+liste.insert(2,"Marc")
+
+#exercice 3
+t = [ ]
+for i in range (10):
+     t.append(i**2)
+    
+     
+
+#exercie4
+     
+t2 = [0]*10
+
+for i in range(len(t2)):
+    t2[i] = 10 - i
+    
+
+
+#exercice5

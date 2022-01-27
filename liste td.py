dc07c155a654a60bb334053b8347bef2ae806f5a
yanisOuali21 <92925423+yanisOuali21@users.noreply.github.com>
2022-01-27 14:38:24 +0100

    Add files via upload

---
+++ liste td.py
@@ -0,0 +1,28 @@
+#exercice6
+t=['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre']
+
+
+def affiche_liste(nom_de_la_liste):
+       for i in nom_de_la_liste :
+           print("" ,i , end="")
+           
+           
+#exercice7
+
+Liste_entier=[23,45,43,7,66,21,200]
+resultantlist = []
+def enleveDoublon(liste):
+    for element in liste:
+        if element not in resultantlist:
+            resultantlist.append(element)
+    
+    print(resultantlist)
+    
+    
+#exercice8
+
+
+
+           
+       
+       

27aa2b9ff9f8985e169eecb0c8af0ea919a2098c
yanisOuali21 <92925423+yanisOuali21@users.noreply.github.com>
2022-03-03 14:37:07 +0100

    Add files via upload

---
+++ mise_au_point_du_programme.py
@@ -0,0 +1,23 @@
+#Exercice1
+import math
+
+def calculerDelta(a,b,c):
+   return b*b-4*a*c
+
+def resoudreEquationSecondDegre(a,b,c):
+   delta = calculerDelta(a,b,c)
+   if delta > 0:
+      racineDeDelta=math.sqrt(delta)
+      retour = [(-b-racineDeDelta)/(2*a),(-b+racineDeDelta)/(2*a)]
+   elif delta < 0:
+      retour = []  #liste vide
+   else:
+      retour = [-b/(2*a)] #liste d'un seul élément
+   return retour
+
+def somme(liste):
+    _somme = 0
+    for i in liste:
+        _somme = _somme + i
+    return _somme
+

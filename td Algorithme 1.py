285e797118072f3ecefc1e3470e5cac4348d8c9a
yanisOuali21 <92925423+yanisOuali21@users.noreply.github.com>
2022-05-09 09:35:47 +0200

    Add files via upload

---
+++ td Algorithme 1.py
@@ -0,0 +1,33 @@
+#exercice1
+
+liste1=[1,2,3,4]
+
+def recherche(liste, valeur):
+    trouve = False
+    for element in liste:      
+        if element  == valeur:
+            trouve = True
+    return trouve
+         
+        
+
+
+        
+
+
+          
+
+        
+
+        
+
+        
+
+  
+    
+
+    
+    
+    
+
+    

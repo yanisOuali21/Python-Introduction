660a89473df19bbc9c294bcdb0cb62267987302c
yanisOuali21 <92925423+yanisOuali21@users.noreply.github.com>
2022-05-16 09:44:13 +0200

    Add files via upload

---
+++ puissance 4.py
@@ -0,0 +1,28 @@
+def genere_grille():
+      '''
+    Description : Fonction qui initialise la grille du jeu
+    return grille (lst) : une liste de listes représentant la grille du jeu.
+    '''
+    grille = [ ["0"*6 ]for _ in range(7)]
+    return grille
+
+
+
+ 
+def affiche_grille(grille):
+    
+    '''Description : Fonction qui affiche la grille du jeu à l'envers.
+    case vide = 0, pion rouge = 1, pion jaune=2.
+    Les numéros de colonne de 1 à 7 doivent apparaître en dessous de la grille.
+    grille (lst) = la liste des listes représentant la grille du jeu
+    return (None)  
+    '''
+    
+    
+
+
+
+
+
+
+    

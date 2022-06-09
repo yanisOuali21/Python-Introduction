58133a310ffdb7ca93ecf430b7dcdfebf56c98e8
yanisOuali21 <92925423+yanisOuali21@users.noreply.github.com>
2022-06-09 14:33:16 +0200

    Add files via upload

--- p4.py
+++ p4.py
@@ -1,19 +1,43 @@
-ligne=6
-colonne=7
-tableau =[[0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0]]
+def genere_grille():
+    grille = [[0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0]]
+    return grille
+    
+def affiche_grille(grille):
+    for k in range(6):
+        print(grille[5-k])
+    print (' 0  1  2  3  4  5  6 ')
+    print (21* '_')
+    
 
-def afficheTableau():
-    for l in range (ligne):
-        for c in range (colonne) :
-            print (tableau[l][c] , end = " " )
-        print()
-        
-        
-def diagonale():
-    for i in range (ligne-3):
-        cpt=0
-        for c in range (colonne-1):
-            if (tableau[l][c] == tableau[l+c][c]) and tableau[l][c] != '.':
-                cpt = cpt+1
-                if cpt ==3:
-                    print (" c est gagner " )
+def grille_pleine(grille):
+    
+    grille_remplie = True
+    for k in grille :
+        for valeur in k :
+            if valeur == 0 :
+                grille_remplie = False
+                break
+        return grille_remplie
+    
+
+def alignement_horizontal(grille,position,joueur):
+    lst = position[0]
+    i = position [1]
+    nbc = len(grille[lst])
+    cpt = 0
+    alignement = False
+    while cpt != 4 and nbc > 0:
+        for k in grille [lst]:
+            if cpt == 4:
+                alignement = True
+            elif k == joueur :
+                cpt += 1
+                nbc -=1
+            else :
+                cpt = 0
+                nbc -=1
+        return alignement
+            
+
+
+
